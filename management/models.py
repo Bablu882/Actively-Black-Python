17e87a1968288ef388c9e2d84ef72c9136f906b8
bablu kumar <bablu.kumar@webnyxa.tech>
2022-08-18 19:04:17 +0530

    user profile done !

--- management/models.py
+++ management/models.py
@@ -1,3 +1,6 @@
+from email.policy import default
+from operator import truediv
+from pickle import TRUE
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save 
@@ -6,9 +9,11 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)        
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    desc=models.TextField(max_length=500)        
     token=models.CharField(max_length=50)
     is_verified=models.BooleanField(default=False)
+    avatar=models.ImageField(null=True,blank=True,default='default.png',upload_to='avatars')
     
 class Forget_Password(models.Model):
     email=models.EmailField(max_length=50)
